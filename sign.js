bb42a059505bd30d72942fa866a1646dff2185f5
Thomas Haller <Thomas.Haller@lab10.coop>
2019-03-26 13:19:36 +0100

    switched to sigma1

--- sign.js
+++ sign.js
@@ -91,13 +91,13 @@ async function sign(web3, rawTx, secondTx) {
   return toHex(serializedTx);
 }
 
-const web3 = new Web3('ws://ws.tau1.artis.network');
+const web3 = new Web3('wss://ws.sigma1.artis.network');
 
 const rawTxOpen = {
  // nonce: '0x00',
   gasPrice: 1000000000,
   gasLimit: '0x50000',
-  to: '0xF6eF10E21166cf2e33DB070AFfe262F90365e8D4',
+  to: '0xE53BA69C94b657838B2b22B9BC609163cC34512f',
   value: 0,
   data: '0x0905186e00000000000000000000000001019e15b7beef611ac4659e7acdc272c4d90afa00000000000000000000000000000000000000000000000000000a86cc92e3da'
 };
@@ -106,7 +106,7 @@ const rawTxClose = {
  // nonce: '0x00',
   gasPrice: 1000000000,
   gasLimit: '0x50000',
-  to: '0xF6eF10E21166cf2e33DB070AFfe262F90365e8D4',
+  to: '0xE53BA69C94b657838B2b22B9BC609163cC34512f',
   value: 0,
   data: '0x9abe837900000000000000000000000001019e15b7beef611ac4659e7acdc272c4d90afa'
 };
